d9c6a736c5d71928cf04569a3fffcaf87a1999ef
Marquis Kurt <software@marquiskurt.net>
2018-08-27 23:51:29 -0400

    Add IDEA files and first part

---
+++ part1/project.py
@@ -0,0 +1,51 @@
+# Variable Types and Functions (Basic)
+# To ease the process, please only write code in marked areas.
+
+# Bool can be True or False
+boolean_value = False
+
+def play_integers():
+    # Variable Types
+    integer = 1
+    float_number = 1.00
+
+    # Note that integer is now a different value.
+    integer = integer + 3
+    print(integer)
+
+    # To increment by one, do the following:
+    integer += 1
+    print(integer)
+
+    # Note that you cannot add integers and floats in other languages.
+    try:
+        print(float_number + integer)
+    except Exception as e:
+        print(e)
+
+
+def concat_string():
+    # Note that the string also has an '\n', meaning a new line.
+    string = "This is a string.\nStrings can have new lines, making paragraphs."
+    number = 16
+    try:
+        print(string + number)
+        # Why does this not work?
+    except Exception as e:
+        print(e)
+
+    # This works though!
+    print(string + str(number))
+
+
+# This function takes parameters. Params work the same way as variables.
+# This function in particular will produce something that can be stored
+# in a variable.
+def multiply(x, y):
+    return x * y
+
+if __name__ == "__main__":
+    play_integers()
+    concat_string()
+    product = multiply(2, 3)
+    print(product)
---
+++ part1/project_test.py
@@ -0,0 +1,11 @@
+# Try It!
+# Use the following space to write a function that does the following:
+# 1. Takes two inputs, 'banana_count' and 'tomato_count'.
+# 2. Print out the total amount of bananas.
+# 3. Print out the total amount of tomatoes.
+# 4. Have the function return the total amount of items.
+
+
+if __name__ == "__main__":
+    # Replace the following line with your function call.
+    exit()
